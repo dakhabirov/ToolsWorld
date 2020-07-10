6318a3e277e9ef1e78692acf093296a92bc5d51e
daniveloper <anix-rp@mail.ru>
2020-07-10 16:12:57 +0300

    first commit

---
+++ ToolsWorld.BL/Model/DbContextInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+
+namespace ToolsWorld.BL.Model
+{
+    class DbContextInitializer : DropCreateDatabaseIfModelChanges<MyDbContext>
+    {
+        /// <summary>
+        /// Инициализация базы данных.
+        /// </summary>
+        /// <param name="dbContext">База данных.</param>
+        protected override void Seed(MyDbContext dbContext)
+        {
+            Worker adm = new Worker
+            {
+                Log = "admin",
+                Fullname = "Администратор",
+                Pass = "12345",
+                Lvl = 777
+            };
+
+            dbContext.Workers.Add(adm);
+            dbContext.SaveChanges();
+            dbContext.Dispose();
+        }
+    }
+}
